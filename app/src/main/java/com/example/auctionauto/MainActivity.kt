080ba874b4777cdd4668374efc49e53a72cffa79
house <spthouse16@gmail.com>
2025-10-22 18:01:11 -0500

    Start of Login

--- app/src/main/java/com/example/auctionauto/MainActivity.kt
+++ app/src/main/java/com/example/auctionauto/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.sp
 import com.example.auctionauto.ui.theme.AuctionAutoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +27,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             AuctionAutoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text("Welcome to Auction Auto", modifier = Modifier.align(Alignment.Center)
+                            .padding(innerPadding)
+                            .offset(y= -100.dp),
+                            fontSize = 20.sp
+                        )
+                        Text("Please Login", modifier = Modifier
+                            .padding(innerPadding)
+                            .offset(y=-75.dp),
+                            fontSize = 20.sp
+                        )
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AuctionAutoTheme {
-        Greeting("Android")
     }
 }
