f9d268b81a227069867e3903776c261e0c47c1c7
house <spthouse16@gmail.com>
2025-10-22 18:36:11 -0500

    Initial Login Screen No functionality

--- app/src/main/java/com/example/auctionauto/MainActivity.kt
+++ app/src/main/java/com/example/auctionauto/MainActivity.kt
@@ -10,19 +10,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import com.example.auctionauto.ui.theme.AuctionAutoTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var Email = ""
+        var Password = ""
+
         enableEdgeToEdge()
         setContent {
             AuctionAutoTheme {
@@ -43,6 +53,46 @@ class MainActivity : ComponentActivity() {
                             .offset(y=-75.dp),
                             fontSize = 20.sp
                         )
+                        TextField(
+                            value = Email,
+                            onValueChange = {Email = it},
+                            label = { Text("Email") }
+                        )
+                        TextField(
+                            value = Password,
+                            onValueChange = {Password = it},
+                            label = { Text("Password") },
+                            modifier = Modifier.padding(innerPadding)
+                                .offset(y=75.dp)
+                        )
+                        Button(onClick = {}, modifier = Modifier
+                            .padding(innerPadding)
+                            .width(300.dp)
+                            .height(50.dp)
+                            .offset(y=160.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xFF87A96B)
+                            )
+
+                        )
+                        {
+                            Text("Login")
+
+                        }
+                        Button(onClick = {},
+                            modifier = Modifier
+                            .padding(innerPadding)
+                            .width(300.dp)
+                            .height(50.dp)
+                            .offset(y=225.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xFF87A96B)
+                            )
+                        )
+                        {
+                            Text("Register")
+
+                        }
                     }
 
                 }
---
+++ app/src/main/java/com/example/auctionauto/User.kt
@@ -0,0 +1,7 @@
+package com.example.auctionauto
+
+class User {
+    var email = ""
+    var password = ""
+    var type = ""
+}
