b53f868ae87df763c70efb11bf5f6fc834301f69
Ianizer <butterballs88@yahoo.com>
2025-11-30 12:00:36 -0600

    fixed imports that broke for some reason...

--- app/src/main/java/com/example/auctionauto/screens/DashboardUI.kt
+++ app/src/main/java/com/example/auctionauto/screens/DashboardUI.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.auctionauto.ListingVMFactory
 import com.example.auctionauto.ListingViewModel
+import com.example.auctionauto.R
 import com.example.auctionauto.data.AppDatabase
 import com.example.auctionauto.data.ListingRepo
 import androidx.compose.foundation.lazy.items
